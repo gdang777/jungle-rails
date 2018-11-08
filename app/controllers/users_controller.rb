86eed0e4a06f9932f608afbb0c1f3464b476d4e0
Gaurav <netgary7@gmail.com>
2018-11-08 23:21:20 +0000

    User registration page added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params)
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to '/', notice: 'User created'
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   end
 
   private
+  def user_params
   params.require(:user).permit(
     :first_name,
     :last_name,
@@ -21,5 +22,6 @@ class UsersController < ApplicationController
     :password,
     :password_confirmation
   )
+  end
 
 end
