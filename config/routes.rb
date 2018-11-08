180ad4655a28cb86842f26bc1de9dbbec5e0df59
Gaurav <netgary7@gmail.com>
2018-11-08 21:11:27 +0000

    User controller/empty view added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  resources :users
+
+  # # get 'signup',
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
