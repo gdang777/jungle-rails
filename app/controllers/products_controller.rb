3fe830f283419469505687883f9ea780b5fa4166
Gaurav <netgary7@gmail.com>
2018-11-08 19:29:16 +0000

    Completed Admin categories feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,34 @@ class ProductsController < ApplicationController
     @product = Product.find params[:id]
   end
 
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to [:admin, :products], notice: 'Product created'
+    else
+      render :new
+    end
+  end
+
+  # def destroy
+  #   @product = Product.find params[:id]
+  #   @product.destroy
+  #   redirect_to [:admin, :products], notice: 'Product Deleted'
+  # end
+
+  # private
+
+  # def product_params
+  #   params.require(:product).permit(
+  #     :name,
+  #     :description,
+  #     :category_id,
+  #     :quantity,
+  #     :image,
+  #     :price
+
+  #   )
+  # end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  config.action_view.raise_on_missing_translations = true
+  # config.action_view.raise_on_missing_translations = true
 
   config.web_console.whitelisted_ips = ['10.0.2.0/24']
 end
